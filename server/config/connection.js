741c6f86737be24e4dff2a7df6dda05ef0cc2888
Gustavo Garcia <gustavogarcia42@yahoo.com>
2022-01-23 14:50:46 -0600

    changed mongo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Books", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/BooksNStuff", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,

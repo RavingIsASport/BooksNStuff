5304774cec91880cda20960582fd35e6be9430dc
Gustavo Garcia <gustavogarcia42@yahoo.com>
2022-01-23 14:44:36 -0600

    modified sign up form and BCS Tutor helped me debug

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -12,7 +12,7 @@ export const LOGIN_USER = gql`
 
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(addUser(username: $username, email:$email , password:$password ) {
+    addUser(username: $username, email: $email, password: $password) {
       _id
       username
       email
